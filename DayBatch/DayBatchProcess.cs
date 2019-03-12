e8194644605269f0bb5f9502eb49dbeb88f0edd7
xjsxjs197 <31617363+xjsxjs197@users.noreply.github.com>
2019-03-12 10:53:30 +0800

    xjsxjs197

--- DayBatch/DayBatchProcess.cs
+++ DayBatch/DayBatchProcess.cs
@@ -666,7 +666,7 @@ namespace DayBatch
             // 获取15分钟数据
             if (hasM15)
             {
-                this.GetMinuteData(TimeRange.M15);
+                //this.GetMinuteData(TimeRange.M15);
             }
 
             // 获取5分钟数据
@@ -686,29 +686,15 @@ namespace DayBatch
                 File.AppendAllText(logFile, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " 获取" + timeRange.ToString() + "数据 开始\r\n", Encoding.UTF8);
 
                 // 设定结束日期
-                DateTime now = DateTime.Now;
+                DateTime now = this.GetNotWeeklyDayDate();
                 string endDay;
                 if (timeRange == TimeRange.Day)
                 {
-                    if (DateTime.Now.Hour >= 15)
-                    {
-                        endDay = now.ToString("yyyy-MM-dd");
-                    }
-                    else
-                    {
-                        endDay = now.AddDays(-1).ToString("yyyy-MM-dd");
-                    }
+                    endDay = now.ToString("yyyy-MM-dd");
                 }
                 else
                 {
-                    if (DateTime.Now.Hour >= 15)
-                    {
-                        endDay = now.ToString("yyyy-MM-dd 15:00:00");
-                    }
-                    else
-                    {
-                        endDay = now.AddDays(-1).ToString("yyyy-MM-dd 15:00:00");
-                    }
+                    endDay = now.ToString("yyyy-MM-dd 15:00:00");
                 }
 
                 // 取得分钟级别数据的共通
@@ -825,7 +811,7 @@ namespace DayBatch
             // 画15分钟趋势图
             if (hasM15)
             {
-                this.DrawQushiImg(TimeRange.M15);
+                //this.DrawQushiImg(TimeRange.M15);
             }
 
             // 画30分钟趋势图
@@ -1072,6 +1058,67 @@ namespace DayBatch
             }
 
             return DateTime.ParseExact(date, dateFormat, System.Globalization.CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// 取得非节假日的时间
+        /// </summary>
+        /// <returns></returns>
+        private DateTime GetNotWeeklyDayDate()
+        {
+            DateTime dt = DateTime.Now;
+
+            if (dt.Hour < 15)
+            {
+                dt = dt.AddDays(-1);
+            }
+
+            while (this.IsHolidayByDate(dt))
+            {
+                dt = dt.AddDays(-1);
+            }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// 判断是不是周末/节假日
+        /// </summary>
+        /// <param name="date">日期</param>
+        /// <returns>周末和节假日返回true，工作日返回false</returns>
+        private bool IsHolidayByDate(DateTime date)
+        {
+            var isHoliday = false;
+            var webClient = new System.Net.WebClient();
+            var PostVars = new System.Collections.Specialized.NameValueCollection
+            {
+                { "d", date.ToString("yyyyMMdd") }//参数
+            };
+            try
+            {
+                var day = date.DayOfWeek;
+
+                // 判断是否为周末
+                if (day == DayOfWeek.Sunday || day == DayOfWeek.Saturday)
+                {
+                    return true;
+                }
+
+                // 0为工作日，1为周末，2为法定节假日
+                var result = Util.HttpPost("http://tool.bitefu.net/jiari/", "d=" + date.ToString("yyyyMMdd"));
+                //var byteResult = webClient.UploadData("http://tool.bitefu.net/jiari/", "POST", PostVars); // 请求地址, 传参方式,参数集合
+                //var result = Encoding.UTF8.GetString(byteResult);//获取返回值
+                if (result == "1" || result == "2") 
+                {
+                    isHoliday = true;
+                }
+            }
+            catch
+            {
+                isHoliday = false;
+            }
+
+            return isHoliday;
         }
 
         #region " 分型处理相关 "
